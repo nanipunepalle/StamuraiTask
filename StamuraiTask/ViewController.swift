e52a3d4becb32097d497fc3fb5abcfbc90db7f4f
Lalith <lalithpunepalli@gmail.com>
2020-04-08 11:41:10 +0530

    Initial Commit

---
+++ StamuraiTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StamuraiTask
+//
+//  Created by Lalith on 08/04/20.
+//  Copyright © 2020 NANI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
